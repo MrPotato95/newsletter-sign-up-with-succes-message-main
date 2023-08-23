b4195e5380b34d64b54c11adcc5d892d43ab0cf3
Radu <paladaradu95@.gmail.com>
2023-08-23 18:43:38 +0300

    fixed htnl

--- script.js
+++
@@ -1,6 +0,0 @@
-const card1 = document.querySelector(".card-1")
-const card2 = document.querySelector(".card-2")
-const form = document.querySelector("#form")
-const emailInput = document.querySelector("#password-input")
-const errorLabel = document.querySelector("#email-error")
-
